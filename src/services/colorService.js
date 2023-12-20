931ff397715b1d8088bd38fb54edbcc80a46be1b
Emopusta <emreduman225@hotmail.com>
2023-12-20 18:04:11 +0300

    added color service

---
+++ src/services/colorService.js
@@ -0,0 +1,15 @@
+import { Color } from "../models/colorModel.js"
+
+
+
+    export function getAllColors(){
+        return Color.findAll();
+    }
+
+    export function createColor(colorName){
+        return Color.create({ name:colorName });
+    }
+
+
+
+
