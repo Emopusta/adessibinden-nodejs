e55810c9f8bd0df22183bda51dd7ae6a57cf4da9
Emopusta <emreduman225@hotmail.com>
2023-12-21 16:13:31 +0300

    refactored

--- src/controllers/colorController.js
+++ src/controllers/colorController.js
@@ -1,14 +1,15 @@
 
 
-     export async function getAllColors(req, res){
+     async function getAllColors(req, res){
         const response = req.service.getAllColors();
         res.status(400).json((await response))
     }
     
-    export async function createColor(req, res){
+    async function createColor(req, res){
         const {name} = req.body;
         const response = req.service.createColor(name);
 
         res.status(400).json((await response).dataValues)
     }
     
+    export {getAllColors, createColor};
