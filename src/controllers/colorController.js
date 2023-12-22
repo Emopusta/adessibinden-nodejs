aa682c42762aa24b803ab6fe5e9746c219ee4c7a
Emopusta <emreduman225@hotmail.com>
2023-12-22 15:23:13 +0300

    added asyncHandler

--- src/controllers/colorController.js
+++ src/controllers/colorController.js
@@ -1,15 +1,16 @@
+import asyncHandler from "../middlewares/asyncHandler.js";
 
 
-     async function getAllColors(req, res){
+    const getAllColors = asyncHandler(async function (req, res){
         const response = req.colorService.getAllColors();
         res.status(400).json((await response))
-    }
+    })
     
-    async function createColor(req, res){
+    const createColor = asyncHandler(async function (req, res){
         const {name} = req.body;
         const response = req.colorService.createColor(name);
 
         res.status(400).json((await response).dataValues)
-    }
+    })
     
     export {getAllColors, createColor};
