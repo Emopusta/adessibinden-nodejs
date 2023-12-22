260bbbce14d942a97929e4b506facdf2396d6b46
Emopusta <emreduman225@hotmail.com>
2023-12-22 14:44:18 +0300

    added auth to get-colors route

--- src/routes/colorRoutes.js
+++ src/routes/colorRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import {getAllColors, createColor} from "../controllers/colorController.js"
+import { authorizeUser } from "../middlewares/authorizeUser.js";
 
 const router = express.Router();
 
-
-router.get("/get-colors", getAllColors);
+router.get("/get-colors", authorizeUser, getAllColors);
 router.post("/create-color", createColor);
 
 
