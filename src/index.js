1e20ee0e2b6d8654efd4dbaf5f390aa293b8e712
Emopusta <emreduman225@hotmail.com>
2023-12-22 14:44:00 +0300

    registered new services

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import colorRoutes from "./routes/colorRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 import asyncHandler from "./middlewares/asyncHandler.js";
 import serviceRegistrer from "./middlewares/serviceRegistrer.js";
+import errorMiddleware from "./middlewares/errorMiddleware.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const port = process.env.PORT || 8000;
@@ -12,19 +15,23 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
+app.use(cookieParser())
 
-app.use("/api", asyncHandler)
 app.use(serviceRegistrer)
+app.use("/api/colors", colorRoutes);
+app.use("/api/auth", authRoutes);
+
 
 app.get("/", async function(req, res){
     res.send("adessibinden backend...");
 })
 
-app.use("/api/colors", colorRoutes);
-
 app.use(function (req, res, next){
     res.status(404).send('<h1>Page not found</h1>')
-
+    
 })
+
+app.use(errorMiddleware);
+
 
 app.listen(port, function(){ console.log(`Server is running on port ${port}`)});
