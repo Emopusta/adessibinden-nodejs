4b2810610761f9c4f59d24d75f686bcb44e430e2
Emopusta <emreduman225@hotmail.com>
2023-12-22 14:43:29 +0300

    added error middleware

---
+++ src/middlewares/errorMiddleware.js
@@ -0,0 +1,13 @@
+async function errorMiddleware(err, req, res, next){
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode: statusCode,
+        message: message,
+    });
+
+}
+
+
+export default errorMiddleware
