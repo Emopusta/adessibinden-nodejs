f8e3c2b2b2bbd7a536dbecba866b57de14a1f3a5
Emopusta <emreduman225@hotmail.com>
2023-12-26 15:22:33 +0300

    Updated error message

--- src/middlewares/authorizeUser.js
+++ src/middlewares/authorizeUser.js
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken';
 export function authorizeUser(req, res, next){
     const token = req.cookies.token;
 
-    if (!token) return next(errorHandler(401, "Unauthorized."))
+    if (!token) return next(errorHandler(401, "Unauthenticated."))
 
     jwt.verify(token, process.env.JWT_SECRET, function(err, user){
         if (err) return next(errorHandler(403, 'Forbidden'));
