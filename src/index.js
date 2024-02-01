736079317f084e0637a0ef401e7bb64a6ddb6648
Emopusta <emreduman225@hotmail.com>
2024-02-01 15:22:04 +0300

    added cors

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import serviceRegistrer from "./middlewares/serviceRegistrer.js";
 import errorMiddleware from "./middlewares/errorMiddleware.js";
 import cookieParser from "cookie-parser";
 import { sequelize } from "./config/database.js";
+import cors from 'cors';
 
 dotenv.config();
 const port = process.env.PORT || 8000;
@@ -13,13 +14,14 @@ const port = process.env.PORT || 8000;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({origin: 'https://localhost:44342', credentials:true}));
 
 app.use(cookieParser())
 
 app.use(serviceRegistrer)
 app.use("/api/colors", colorRoutes);
-app.use("/api/auth", authRoutes);
+app.use("/api/Auth", authRoutes);
 
 
 app.get("/", async function(req, res){
@@ -30,9 +32,8 @@ app.use(function (req, res, next){
     res.status(404).send('<h1>Page not found</h1>')
     
 })
-
 app.use(errorMiddleware);
 
-sequelize.sync().catch(err => console.log(err));
+// sequelize.sync().catch(err => console.log(err));
 
 app.listen(port, function(){ console.log(`Server is running on port ${port}`)});
