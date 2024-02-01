78052b4c1e2973560d67842fdbfaf5c00a48b257
Emopusta <emreduman225@hotmail.com>
2024-02-01 15:20:52 +0300

    fixed error data result in middleware

--- src/middlewares/errorMiddleware.js
+++ src/middlewares/errorMiddleware.js
@@ -2,9 +2,12 @@ async function errorMiddleware(err, req, res, next){
     const statusCode = err.statusCode || 500;
     const message = err.message || "Internal Server Error";
     return res.status(statusCode).json({
+        data: null,
+        error: {
+            Detail: message,
+            StatusCode: statusCode
+        },
         success: false,
-        statusCode: statusCode,
-        message: message,
     });
 
 }
