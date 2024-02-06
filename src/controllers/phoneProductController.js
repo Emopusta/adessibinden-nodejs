18a4218b6447802f8d32cda9501016edcc7e4f2c
Emopusta <emreduman225@hotmail.com>
2024-02-06 13:28:16 +0300

    updated phone product get by id service

--- src/controllers/phoneProductController.js
+++ src/controllers/phoneProductController.js
@@ -3,7 +3,7 @@ import asyncHandler from "../middlewares/asyncHandler.js";
 const getPhoneProductByProductId = asyncHandler(async function(req, res){
     const productId = parseInt(req.params.productId);
     const response = req.phoneProductService.getPhoneProductByProductId(productId);
-    res.status(200).json({data:(await response).dataValues, error:null, success:true})
+    res.status(200).json({data:(await response), error:null, success:true})
 })
 
 
--- src/services/phoneProductService.js
+++ src/services/phoneProductService.js
@@ -1,7 +1,37 @@
-import PhoneProduct from '../models/phoneProduct.js'
+import Color from '../models/colorModel.js'
+import PhoneInternalMemory from '../models/phoneInternalMemory.js';
+import PhoneModel from '../models/phoneModelModel.js';
+import PhoneProduct from '../models/phoneProductModel.js'
+import PhoneRAM from '../models/phoneRAMModel.js';
+import Product from '../models/productModel.js';
+import User from '../models/userModel.js';
 
-function getPhoneProductByProductId(productId){
-    return PhoneProduct.findOne({where: { ProductId : productId }})
+async function getPhoneProductByProductId(productId){
+    const phoneProduct = PhoneProduct.findOne({
+        where: { ProductId : productId },
+        include:[
+            {model: Color, as: 'Color'},
+            {model: Product, as: 'Product' , include: [{model: User, as: 'CreatorUser'}]},
+            {model: PhoneModel, as: 'Model'},
+            {model: PhoneRAM, as: 'RAM'},
+            {model: PhoneInternalMemory, as: 'InternalMemory'},
+        ]});
+
+    const {Color: color, Product: product,Model: phoneModel, RAM: phoneRAM, InternalMemory: phoneInternalMemory,  ...rest} = (await phoneProduct).dataValues;
+
+    const result = { 
+        ProductDescription: product.Description,
+        ProductTitle: product.Title,
+        ProductCreatorUserEmail: product.CreatorUser.Email,
+        ProductCreatorUserId: product.CreatorUserId,
+        ColorName: color.Name,
+        ModelName: phoneModel.Name,
+        InternalMemoryCapacity: phoneInternalMemory.Capacity,
+        RAMMemory: phoneRAM.Memory, 
+        UsageStatus: rest.UsageStatus,
+        Price: Number(rest.Price.replace(/[^0-9.-]+/g,"")),
+    }
+    return result;
 }
 
 export default function phoneProductService(){
