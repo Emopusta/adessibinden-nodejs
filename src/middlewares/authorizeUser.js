d6bdd78852a12bec0ddba2f61e2cff2a89b3d79e
Emopusta <emreduman225@hotmail.com>
2024-03-21 11:12:14 +0300

    Bugfix authorization Unauthenticated check.

--- src/middlewares/authorizeUser.js
+++ src/middlewares/authorizeUser.js
@@ -3,9 +3,9 @@ import jwt from 'jsonwebtoken';
 
 export function authorizeUser(req, res, next){
     const authorization = req.header('Authorization')
+    if (!authorization) return next(errorHandler(403, "Unauthenticated."))
+    
     const [bearerString, token] = authorization.split(' ');
-
-    if (!token) return next(errorHandler(403, "Unauthenticated."))
 
     jwt.verify(token, process.env.JWT_SECRET, function(err, user){
         if (err) return next(errorHandler(401, 'Unauthorized.'));
